4a9a6098801b50f97acc51968ff310b5bbeaa2f5
Andrew Reid <Andrew Reid>
2016-06-12 18:07:47 -0700

    support for added route

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var gameMatch = require ('../node_modules/colorGen/color');
+
 var app = express();
 var router = express.Router();
 
@@ -10,31 +11,27 @@ router.route('/')
     var newGame = req.body;
     var playerName = newGame.name;
     var playerColors = [newGame.box_0, newGame.box_1, newGame.box_2, newGame.box_3];
+    var score = 20;
     console.log(playerName + ',' + playerColors + ',');
+    console.log(newGame.highScore);
 
     if (!newGame) {
         res.status(400).send('error sending game to server');
     }
 
     var serverColors = gameMatch.getColors(playerColors);
+    var score = gameMatch.getScore(playerColors, serverColors);
 
-    // Might need to put the subroutine here
-    var score = gameMatch.getScore(playerColors, serverColors)
-    // console.log('Player ROUTE: ' + playerColors);
+    console.log('Total Score :' + score);
+    serverColors.push(score);
     console.log('Server ROUTE: ' + serverColors);
-    console.log('Score :' + score);
-    // set the backbone values here???
+    
+
     res.send(serverColors);
-    // = foodTrucks.addTruck(newTruck);
 
 })
-
 .get(function (req, res) {
-    var serverColors = gameMatch.getColors();
-    // console.log(serverColors);
-    res.send(serverColors);
-    // res.json({ colors: array });
-    // trying to get rid of Can't set headers error
+
 });
 
 module.exports = router;
