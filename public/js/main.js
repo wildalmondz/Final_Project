c07e03bfe09ed7b26e3ebad302d44b8fe096c955
Andrew Reid <Andrew Reid>
2016-06-14 20:03:11 -0700

    incorporated feedback for final

--- public/js/main.js
+++ public/js/main.js
@@ -4,27 +4,27 @@
 /* eslint-disable no-undef */
 
 /* eslint-disable no-console */
+/* eslint-disable comma-dangle */
 
 $(init);
 // var play;
-  var colorElements = [0, 0, 0, 0];
+var colorElements = [0, 0, 0, 0];
 
-  function init() {
-    document.getElementsByClassName('colors')[0].style.visibility = 'hidden';
-    var saveButton = document.getElementById('save');
-    saveButton.addEventListener('click', saveItem);
-  }
+function init() {
+  document.getElementsByClassName('colors')[0].style.visibility = 'hidden';
+  var saveButton = document.getElementById('save');
+  saveButton.addEventListener('click', saveItem);
+}
 
-  function submitColors() {
-    var play = new Games({});
+function submitColors() {
+  var play = new Games({});
 
-    var view = new View({ model: play });
-    var total = new totalView({ model: play });
-    var boxView = new Box0View({ model: play });
-    var boxView = new Box1View({ model: play });
-    var boxView = new Box2View({ model: play });
-    var boxView = new Box3View({ model: play });
-    var curGame = play.create(player.attributes);
-    console.log(curGame);
-  }
- 
+  var view = new View({ model: play });
+  var total = new totalView({ model: play });
+  var box0View = new Box0View({ model: play });
+  var box1View = new Box1View({ model: play });
+  var box2View = new Box2View({ model: play });
+  var box3View = new Box3View({ model: play });
+  var curGame = play.create(player.attributes);
+  console.log(curGame);
+}
