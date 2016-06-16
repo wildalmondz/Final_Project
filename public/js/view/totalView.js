72ae056762a1d3e9e4cffa0b41e162c275509a25
Andrew Reid <Andrew Reid>
2016-06-16 12:26:59 -0700

    Backbone components modules

--- public/js/view/totalView.js
+++ public/js/view/totalView.js
@@ -5,23 +5,26 @@
 
 /* eslint-disable no-console */
 
-var totalView = Backbone.View.extend({
-  initialize: function () {
-    console.log('Total View is initialized');
-    this.template = $('#total-template').children();
-    this.render();
-  },
-  el: '#total',
-  render: function () {
-    console.log('Total render' + this.model);
-    this.listenTo(this.model, 'change', function () {
-      console.log('Total detected');
-      var totalScore = 'Total: ' + this.model.at(1).attributes[5];
-      // var highScore = player.get('highScore');
-      console.log('Total score ' + totalScore);
-      this.$el.find('ul').append(totalScore);
-      Backbone.history.navigate('done',  {trigger: true});
-    });
-  }
-});
-
+// var totalView = Backbone.View.extend({
+var totalView = (function () {
+  return Backbone.View.extend({
+    initialize: function () {
+      console.log('Total View is initialized');
+      this.template = $('#total-template').children();
+      this.render();
+    },
+    el: '#total',
+    render: function () {
+      console.log('Total render' + this.model);
+      this.listenTo(this.model, 'change', function () {
+        console.log('Total detected');
+        var totalScore = 'Total: ' + this.model.at(1).attributes[5];
+        // var highScore = player.get('highScore');
+        console.log('Total score ' + totalScore);
+        this.$el.find('ul').append(totalScore);
+        /* eslint-disable comma-dangle */
+        Backbone.history.navigate('done',  { trigger: true });
+      });
+    }
+  });
+})();
